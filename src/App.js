e418b85620ad12fa1b619ed789f6200b8a87624f
Saman <samangithoseinpour@gmail.com>
2023-03-08 21:39:47 +0330

    fix--infinity-request-causeOf-useEffect

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import { useEffect, useContext } from 'react';
-import BookCreate from './components/BookCreate';
-import BookList from './components/BookList';
-import BooksContext from './context/books';
+import { useEffect, useContext } from "react";
+import BookCreate from "./components/BookCreate";
+import BookList from "./components/BookList";
+import BooksContext from "./context/books";
 
 const App = () => {
   const { fetchBooks } = useContext(BooksContext);
 
   useEffect(() => {
     fetchBooks();
-  });
+  }, []);
 
   return (
     <div className="app">
