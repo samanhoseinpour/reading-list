3b0eb70de1d9f9976b821bd874de281f1e8d22bb
Saman <samangithoseinpour@gmail.com>
2023-09-07 20:13:44 +0330

    fix: rest api

--- src/context/books.js
+++ src/context/books.js
@@ -1,5 +1,5 @@
-import { createContext, useState, useCallback } from "react";
-import axios from "axios";
+import { createContext, useState, useCallback } from 'react';
+import axios from 'axios';
 
 const BooksContext = createContext();
 
@@ -7,15 +7,20 @@ const Provider = ({ children }) => {
   const [books, setBooks] = useState([]);
 
   const fetchBooks = useCallback(async () => {
-    const response = await axios.get("http://localhost:3001/books");
+    const response = await axios.get(
+      'https://readinglist-portfolio.netlify.app/books'
+    );
 
     setBooks(response.data);
   }, []);
 
   const createBook = async (title) => {
-    const response = await axios.post("http://localhost:3001/books", {
-      title,
-    });
+    const response = await axios.post(
+      'https://readinglist-portfolio.netlify.app//books',
+      {
+        title,
+      }
+    );
 
     const updateBooks = [...books, response.data];
 
@@ -23,7 +28,7 @@ const Provider = ({ children }) => {
   };
 
   const deleteBookById = async (id) => {
-    await axios.delete(`http://localhost:3001/books/${id}`);
+    await axios.delete(`https://readinglist-portfolio.netlify.app/${id}`);
 
     const updatedBooks = books.filter((book) => {
       return book.id !== id;
@@ -33,9 +38,12 @@ const Provider = ({ children }) => {
   };
 
   const editBooksById = async (id, newTitle) => {
-    const response = await axios.put(`http://localhost:3001/books/${id}`, {
-      title: newTitle,
-    });
+    const response = await axios.put(
+      `https://readinglist-portfolio.netlify.app/${id}`,
+      {
+        title: newTitle,
+      }
+    );
 
     const updatedBooks = books.map((book) => {
       if (book.id === id) {
